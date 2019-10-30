0f93e7aac59a719737de14572e9d8763b421051e
Noor Schat <creeperlwd@gmail.com>
2019-10-30 15:30:09 +0000

    Update config.fish

--- config.fish
+++ config.fish
@@ -6,18 +6,24 @@
 #    By: nschat <nschat@student.codam.nl>             +#+                      #
 #                                                    +#+                       #
 #    Created: 2019/10/28 17:46:37 by nschat        #+#    #+#                  #
-#    Updated: 2019/10/28 17:46:42 by nschat        ########   odam.nl          #
+#    Updated: 2019/10/30 15:45:57 by nschat        ########   odam.nl          #
 #                                                                              #
 # **************************************************************************** #
 
 # dev alias
-alias gitdiff 'git --no-pager diff master'
 alias vim 'nvim'
 alias vi 'nvim'
-alias diff 'diff --color'
 alias gitdiff 'git --no-pager diff --cached'
+alias listempty 'rg -l -U "\{\n\}|\(void\)"'
+alias norm 'find . -name "*.[c,h]" | xargs norminette'
 
 # unicorn greeting
 function fish_greeting
-	cowsay -f unicorn Welcome to $hostname, $USER | lolcat
+	codam | lolcat
+end
+
+# welcome message
+function codam
+	figlet -m 1 -w 150 -f big "$USER @ codam"
+	printf "Welcome to $hostname, $USER\n\n"
 end
